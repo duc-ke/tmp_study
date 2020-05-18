ba5b0a9878bf869dd59eadd393a4040dd76c689b
Youngihnkwon <youngihnkwon@Youngihnkwonui-MacBookPro.local>
2020-05-18 11:17:56 +0900

    sample code

---
+++ tensor.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+# X and Y data
+
+x_train = [1,2,3]
+y_train = [1,2,3]
+W = tf.Variable(tf.random.normal([1]), name = 'weights')
+b = tf.Variable(tf.random.normal([1]), name = 'bias')
+
+# our hypothesis 
+hypothesis = x_train * W + b
+
+# cost function
+cost = tf.reduce_mean(tf.square(hypothesis - y_train))
